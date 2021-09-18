6ff8b1b2e949cdc6cf41d1ab64387753e8284273
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 14:27:12 -0400

    Add background to sign in page

--- client/src/pages/signin/SignIn.js
+++ client/src/pages/signin/SignIn.js
@@ -2,11 +2,11 @@ import React from 'react';
 import SignInComponent from '../../components/sign-in/SignIn';
 export default function SignIn () {
 	return (
-		<div className="flex flex-wrap h-screen bg-blue-light">
+		<div className="flex flex-wrap h-screen bg-gradient-to-r from-blue-dark to-blue-light">
 			<a className="text-white ml-5 mt-5" href="/">
 				Back
 			</a>
-			<div className="w-full flex h-screen place-items-center bg-blue-light">
+			<div className="w-full flex h-screen place-items-center bg-gradient-to-r from-blue-dark to-blue-light">
 				<div className="ml-6 lg:ml-40">
 					<SignInComponent />
 				</div>
