eee18735f04d83c524f9d4f276ceea334a005902
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 04:02:06 -0400

    Add extra scope

--- client/src/components/sign-in/spotify.js
+++ client/src/components/sign-in/spotify.js
@@ -2,7 +2,7 @@ const authEndpoint = 'https://accounts.spotify.com/authorize';
 const redirectUri = 'http://localhost:3000/';
 const clientId = 'ffbcecc74f594134a14ba26315bb3e70';
 
-const scopes = [ 'streaming', 'user-read-email', 'user-read-private' ];
+const scopes = [ 'streaming', 'user-read-email', 'user-read-private', 'user-modify-playback-state' ];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join(
 	'%20'
