d5e2f712db113105be84c25423057f62cb94f9be
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 01:37:58 -0400

    Authorization

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 require('dotenv').config();
 
 const spotifyAPI = require('spotify-web-api-node');
@@ -11,12 +13,10 @@ const scopes = [
 	'user-read-currently-playing'
 ];
 
-console.log(process.env.CLIENT_ID);
-
 const spotify = new spotifyAPI({
 	clientId     : process.env.CLIENT_ID,
 	clientSecret : process.env.CLIENT_SECRET,
-	redirectUri  : 'http://localhost:4000'
+	redirectUri  : `${process.env.DEV_URL}/callback`
 });
 
 var stateKey = 'spotify_auth_state';
@@ -25,6 +25,24 @@ var auth_string = spotify.createAuthorizeURL(scopes, stateKey);
 console.log(auth_string);
 const main = require('./server/routes/main/main');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/', main);
 
+app.get('/callback', (req, res) => {
+	const code = req.query.code;
+
+	spotify.authorizationCodeGrant(code).then((data) => {
+		console.log('The token is', data.body['access_token']);
+		console.log('The expiry is', data.body['expires_in']);
+
+		spotify.setAccessToken(data.body['access_token']);
+		spotify.setRefreshToken(data.body['refresh_token']);
+	});
+	res.send('auth complete');
+});
+
 app.listen(4000, () => console.log('Listening on 4000'));
+
+module.exports = spotify;
