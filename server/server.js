9ea465b768465801b6e2cc36df5166e787590206
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 06:52:09 -0400

    Created database models

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize-cockroachdb');
 const fs = require('fs');
+const { QueryTypes } = require('sequelize-cockroachdb');
 
 require('dotenv').config();
 
@@ -19,19 +20,26 @@ app.listen(4000, () => {
 	console.log('Listening on 4000');
 });
 
-// var sequelize = new Sequelize({
-// 	dialect        : 'postgres',
-// 	username       : process.env.USERNAME,
-// 	password       : process.env.PASSWORD,
-// 	host           : process.env.DB_HOST,
-// 	port           : process.env.DB_PORT,
-// 	database       : process.env.DATABASE,
-// 	dialectOptions : {
-// 		ssl : {
-// 			rejectUnauthorized : false,
-// 			// For secure connection:
-// 			ca                 : fs.readFileSync('certs/root.crt').toString()
-// 		}
-// 	},
-// 	logging        : false
-// });
+var sequelize = new Sequelize({
+	dialect        : 'postgres',
+	username       : process.env.USERNAME,
+	password       : process.env.PASSWORD,
+	host           : process.env.DB_HOST,
+	port           : process.env.DB_PORT,
+	database       : process.env.DATABASE,
+	dialectOptions : {
+		ssl : {
+			rejectUnauthorized : false,
+			// For secure connection:
+			ca                 : fs.readFileSync('certs/root.crt').toString()
+		}
+	},
+	logging        : false
+});
+
+module.exports = sequelize;
+
+const User = require('./server/models/User');
+
+const user = User.build({ id: '1', name: 'Andres', spotify_id: '1236lol', email: '123@gmail.com', token: '187203' });
+console.log(user);
