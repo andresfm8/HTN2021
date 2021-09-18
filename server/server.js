640285b4ca9695a859d1b5de176d6b316023c3e6
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 01:03:55 -0400

    Set up basic server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
 
+const main = require('./server/routes/main/main');
+
+app.use('/', main);
+
+app.listen(4000, () => console.log('Listening on 4000'));
---
+++ server/server/routes/main/main.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const users = require('./users/main');
+
+router.use('/', users);
+
+module.exports = router;
