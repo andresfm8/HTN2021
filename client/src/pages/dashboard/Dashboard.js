8399dc7dea49c9683aa88e09333cace80595f0ba
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 15:51:14 -0400

    Dash

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import CustomButton from '../../components/custom-button/CustomButton';
 import SearchBox from '../../components/search-box/SearchBox';
-
+import axios from 'axios';
 const DashboardPage = ({ code }) => {
+	const [ accessToken, setAccessToken ] = useState('');
+
+	useEffect(() => {
+		axios.post('http://localhost:4000/login', { code }).then((res) => res.json).then((res) => console.log(res));
+	}, []);
 	return (
 		<div>
 			<SearchBox placeholder="Search songs..." />
