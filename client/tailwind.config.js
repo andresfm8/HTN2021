6a09574ef41567d9319e3519804810268300a843
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 14:45:02 -0400

    Session selection page complete

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
 	purge    : [ './src/**/*.{js,jsx,ts,tsx}', './public/index.html' ],
 	darkMode : false, // or 'media' or 'class'
 	theme    : {
-		extend : {},
+		extend : {
+			outline: {
+				white: '2px solid #FFFFFF'
+			}
+		},
 		colors : {
 			green : {
 				spotify : '#1DB954'
