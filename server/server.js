db4a89b1d9a519616ff7102cb4d5ee38e3e01309
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-18 03:34:24 -0400

    Commits before pulling from main

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
 
 require('dotenv').config();
 
@@ -13,16 +14,18 @@ const scopes = [
 	'user-read-currently-playing'
 ];
 
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = `${process.env.DEV_URL}/callback`;
+
 const spotify = new spotifyAPI({
-	clientId     : process.env.CLIENT_ID,
-	clientSecret : process.env.CLIENT_SECRET,
-	redirectUri  : `${process.env.DEV_URL}/callback`
+	clientId     : client_id,
+	clientSecret : client_secret,
+	redirectUri  : redirect_uri
 });
 
 var stateKey = 'spotify_auth_state';
 
-var auth_string = spotify.createAuthorizeURL(scopes, stateKey);
-console.log(auth_string);
 const main = require('./server/routes/main/main');
 
 app.use(bodyParser.json());
@@ -30,17 +33,51 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', main);
 
+app.get('/login', (req, res) => {
+	var auth_string = spotify.createAuthorizeURL(scopes, stateKey);
+	res.send(`<a>${auth_string}</a>`);
+});
+
 app.get('/callback', (req, res) => {
 	const code = req.query.code;
 
 	spotify.authorizationCodeGrant(code).then((data) => {
-		console.log('The token is', data.body['access_token']);
-		console.log('The expiry is', data.body['expires_in']);
-
-		spotify.setAccessToken(data.body['access_token']);
-		spotify.setRefreshToken(data.body['refresh_token']);
+		access_token = data.body['access_token'];
+		refresh_token = data.body['refresh_token'];
+		res.send({
+			'access-token'  : data.body['access_token'],
+			'refresh-token' : data.body['refresh_token'],
+			refresh       : `/refresh_token?access_token=${data.body['access_token']}&refresh_token=${data.body[
+				'refresh_token'
+			]}`
+		});
 	});
-	res.send('auth complete');
+});
+
+app.get('/refresh_token', (req, res) => {
+	var refresh_token = req.query.refresh_token;
+	var access_token = req.query.access_token;
+
+	var authOptions = {
+		url     : 'https://accounts.spotify.com/api/token',
+		headers : { Authorization: 'Basic ' + new Buffer(client_id + ':' + client_secret).toString('base64') },
+		form    : {
+			grant_type    : 'refresh_token',
+			refresh_token : refresh_token
+		},
+		json    : true
+	};
+
+	request.post(authOptions, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var access_token = body.access_token;
+			res.send({
+				access_token : access_token
+			});
+		} else {
+			res.send('error');
+		}
+	});
 });
 
 app.listen(4000, () => console.log('Listening on 4000'));
