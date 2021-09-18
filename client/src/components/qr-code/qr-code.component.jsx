9494fa810a5771b9b8b86c0668f4fd8d336de34f
Stefan <stefanleoussis@gmail.com>
2021-09-18 04:19:14 -0400

    Set Up QR Code Component

--- client/src/components/qr-code/qr-code.component.jsx
+++ client/src/components/qr-code/qr-code.component.jsx
@@ -1,9 +1,20 @@
-const QRCode = () => {
+import QRCode from "qrcode";
+import { useEffect, useState} from "react";
+
+const QRCodeGenerate = ({link}) => {
+  const [src, setSrc] = useState("");
+
+  useEffect(() => {
+    QRCode.toDataURL(link).then((data) => {
+      setSrc(data);
+    })
+  }, [])
   return (
     <div>
       QR Code Setup here
+      <img src={src} />
     </div>
   )
 }
 
-export default QRCode;
+export default QRCodeGenerate;
