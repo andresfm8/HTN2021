ae97d38394636979032d93d29daa809794c2d603
Andres Munevar <afmunevarm@hotmail.com>
2021-09-18 13:37:05 -0400

    Cleanup dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,16 @@ import SignIn from './pages/signin/SignIn';
 
 const App = () => {
 	return (
-		<Switch>
-			{/* SignIn should have logic to determine if user is already loged in and redirect to sessions page */}
-			{/* All routes except for / should be accessible for auth users only */}
-			<Route exact path="/" component={Landing} />
-			<Route exact path="/signin" component={SignIn} />
-			<Route exact path="/sessions" component={SessionSelectionPage} />
-			<Route exact path="/dashboard" component={DashboardPage} />
-		</Switch>
+		<div>			
+			<Switch>
+				{/* SignIn should have logic to determine if user is already loged in and redirect to sessions page */}
+				{/* All routes except for / should be accessible for auth users only */}
+				<Route exact path="/" component={Landing} />
+				<Route exact path="/signin" component={SignIn} />
+				<Route exact path="/sessions" component={SessionSelectionPage} />
+				<Route exact path="/dashboard" component={DashboardPage} />
+			</Switch>
+		</div>
 	);
 };
 
